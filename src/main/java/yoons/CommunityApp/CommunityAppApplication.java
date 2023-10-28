1c9e3318851b4e7e8755f2ecfaecb69fa6be2312
yooonwodyd <dbswodyd00@naver.com>
2023-10-29 00:48:03 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/yoons/CommunityApp/CommunityAppApplication.java
@@ -0,0 +1,13 @@
+package yoons.CommunityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yoons/CommunityApp/CommunityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package yoons.CommunityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
