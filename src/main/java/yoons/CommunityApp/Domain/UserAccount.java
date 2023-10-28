e6b370090d9200129f85823062545878d1b4db9d
yooonwodyd <dbswodyd00@naver.com>
2023-10-29 02:40:30 +0900

    db 생성을 위해 간단한 userAccount와 article 클래스 작성

--- src/main/java/yoons/CommunityApp/Domain/UserAccount.java
+++ src/main/java/yoons/CommunityApp/Domain/UserAccount.java
@@ -1,2 +1,32 @@
-package yoons.CommunityApp.Domain;public class UserAccount {
+package yoons.CommunityApp.Domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class UserAccount extends AuditingFields{
+    @Id
+    @Column(length = 50)
+    private String userId;
+    @Setter @Column(nullable = false) private String userPassword;
+    @Setter @Column(length = 100) private String nickname;
+
+    protected UserAccount() {
+    }
+
+    private UserAccount(String userId, String userPassword, String nickname) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.nickname = nickname;
+    }
+
+    public UserAccount of(String userId, String userPassword, String nickname){
+        return new UserAccount(userId, userPassword, nickname);
+    }
 }
